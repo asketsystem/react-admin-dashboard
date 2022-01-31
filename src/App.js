553883abe9aef9911240a50d9da5d5a92d2b2e1d
asketsystem <asketsystem1@gmail.com>
2022-01-31 18:35:56 +0000

    Second parts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css'
-import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import NewProduct from './pages/NewProduct';
+import NewUser from './pages/NewUser';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import User from './pages/User';
+import UserList from './pages/UserList';
 
 function App() {
   return (
-    <h1>
-      <NavBar />
-    </h1>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path='/user/:userId'>
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path='/product/:productId'>
+            <Product />
+          </Route>
+          <Route path="/newProduct">
+            <NewProduct />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
